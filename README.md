34c0864dfbdd597210ba00077cc46e97e46f1af3
arthudearaujoneves3 <arthurdearaujoneves333@gmail.com>
2025-02-05 19:14:31 -0300

    Script feito com Python

---
+++ README.md
@@ -0,0 +1,42 @@
+### Dependências
+
+Tenha certeza de ter o [python](https://www.python.org/) instalado em sua máquina
+
+### Instalação do projeto
+
+Clone o projeto no seu computador
+```bash
+git clone 
+```
+Instale as dependências necessárias para o projeto rodar
+```bash
+pip install requirements.txt
+```
+
+### Como adicionar os programas
+
+Vá até o arquivo principal **main.py**, e procure a variável `programs` que está logo no topo do código, e adicione o nome do programa e o **caminho até o executável** dele. 
+
+```python
+"": {
+    "path": r""
+},
+```
+
+> [!NOTE]
+> Não retire o **r** que acompanha o caminho do programa, isso pode ocasionar em *problemas* durante o script.
+
+
+Caso queira testar o funcionamento do script, rode o seguinte comando no terminal
+```bash
+py main.py
+```
+
+### Executar script ao sistema iniciar
+
+Agora iremos fazer com que o script execute toda vez que o windows iniciar 
+
+1) Aperte `win + r` e digite **shell:startup**
+2) Copie e cole o arquivo **main.py** dentro da pasta aberta e altere o formato do arquivo para **.pyw** para que ele possa exeutar sem o console
+
+Se tudo estiver correto, agora o script irá executar toda vez que você iniciar o seu  sistema
---
+++ main.py
@@ -0,0 +1,63 @@
+import subprocess
+import customtkinter
+
+# O 'r' no começo é para transforma a strin bruta, já que a '\' interpreta como formfeed (\f) 
+# Coloque o nome do programe e o caminho para o .exe
+programs = {
+    "Paint.NET": {
+        "path": r"C:\Program Files\Paint.NET\paintdotnet.exe"
+    },
+    "Firefox": {
+        "path": r"C:\Program Files\Mozilla Firefox\firefox.exe"
+    },
+    "Photoshop": {
+        "path": r"C:\Program Files\Mozilla Firefox\firefox.ex"
+    }
+}
+
+colors = {
+    "red": "#ff2c2c",
+    "green": "#0BDA51",
+    "white": "#FFFFFF"
+}
+
+# Tema da janela
+customtkinter.set_appearance_mode("System")
+customtkinter.set_default_color_theme("blue")
+
+# Configurações da tela
+app = customtkinter.CTk()
+app.geometry("400x300")
+app.title("Logs")
+
+# def initializeScreenViewer():
+def getProgram():
+    errors = 0
+
+    # key é o nome do programa
+    for key in programs:
+        errors = openProgram(key, programs[key]["path"], errors)
+    
+    # Se não houver erros, fechamos a janela
+    if errors == 0:
+        return app.quit()
+
+def openProgram(name: str, path: str, errors: int):
+    try:
+        createMessage(text=f"Abrindo programa {name}...", error=False)
+        subprocess.Popen(path)
+    except:
+        createMessage(text=f"{name} não encontrado. Caminho do executável incorreto \n {path}", error=True)
+        errors += 1
+
+    return errors
+
+def createMessage(text: str, error: bool):
+    if error:
+        return customtkinter.CTkLabel(app, wraplength=300, text=text, text_color=colors["white"], bg_color=colors["red"]).pack()
+    
+    return customtkinter.CTkLabel(app, wraplength=300, text=text).pack()
+    
+# Inicio
+getProgram()
+app.mainloop()
